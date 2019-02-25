504b7a3132b23a683c87e21b414d2fd68142a4ff
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-25 22:51:34 +0000

    passing test for leap year

--- src/LeapYears.java
+++ src/LeapYears.java
@@ -1,8 +1,11 @@
 public class LeapYears {
 
     public static String year(int a) {
-        return "It is not a leap year";
+        if ((a % 4 == 0) && (a % 100 != 0) || (a % 400 == 0)) {
+            return "It is a leap year";
+        } else {
+            return "It is not a leap year";
+        }
     }
-
 }
 
