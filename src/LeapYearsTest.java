e97129a22a85c1394b478a079a33991af571b66f
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-25 22:48:05 +0000

    failing first test - if not a leap year

--- src/LeapYearsTest.java
+++ src/LeapYearsTest.java
@@ -4,4 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LeapYearsTest {
 
+    private final LeapYears leapyears = new LeapYears();
+
+    @Test
+    void testOne() {
+        assertEquals("It is not a leap year", leapyears.year(2019));
+    }
+
 }
